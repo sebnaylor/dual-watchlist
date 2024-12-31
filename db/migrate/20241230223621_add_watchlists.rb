94647067d501911cb10d4a1f9b0f5829a4807eff
Seb Naylor <sebnaylor@gmail.com>
2024-12-31 15:30:36 +0000

    migration

---
+++ db/migrate/20241230223621_add_watchlists.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class AddWatchlists < ActiveRecord::Migration[7.2]
+  def change # rubocop:disable Metrics/MethodLength
+    create_table :shared_watchlists do |t|
+      t.string :uuid, null: false, index: { unique: true }
+
+      t.timestamps
+    end
+
+    create_table :personal_watchlists do |t|
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
+      t.references :shared_watchlist, foreign_key: true, index: true
+
+      t.timestamps
+    end
+
+    create_table :personal_watchlist_media_items do |t|
+      t.references :personal_watchlist, null: false, foreign_key: true
+      t.references :media, null: false, foreign_key: true
+    end
+  end
+end
