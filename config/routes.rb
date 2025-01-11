dfad221ca17d8a609b675ecfa84599522545cc67
Seb Naylor <sebnaylor@gmail.com>
2025-01-11 21:55:08 +0000

    try to fix prod issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,27 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_scope :user do
-    # sessions
-    get    'login',  to: 'devise/sessions#new',     as: :new_user_session
-    post   'login',  to: 'devise/sessions#create',  as: :user_session
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
-    get 'logout', to: 'sessions#destroy_via_get', as: :logout_via_get
-    # registrations
-    put    '/account',  to: 'devise/registrations#update'
-    delete '/account',  to: 'devise/registrations#destroy'
-    post   '/account',  to: 'devise/registrations#create'
-    get    '/register', to: 'devise/registrations#new',    as: :new_user_registration
-    get    '/account',  to: 'devise/registrations#edit',   as: :edit_user_registration
-    patch  '/account',  to: 'devise/registrations#update', as: :user_registration
-    get    '/account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
-    # passwords
-    get   'new-pass',  to: 'devise/passwords#new',    as: :new_user_password
-    get   'edit-pass', to: 'devise/passwords#edit',   as: :edit_user_password
-    patch 'edit-pass', to: 'devise/passwords#update', as: :user_password_update
-    post  'new-pass',  to: 'devise/passwords#create', as: :user_password
-  end
+  # devise_scope :user do
+  #   # sessions
+  #   get    'login',  to: 'devise/sessions#new',     as: :new_user_session
+  #   post   'login',  to: 'devise/sessions#create',  as: :user_session
+  #   delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  #   get 'logout', to: 'sessions#destroy_via_get', as: :logout_via_get
+  #   # registrations
+  #   put    '/account',  to: 'devise/registrations#update'
+  #   delete '/account',  to: 'devise/registrations#destroy'
+  #   post   '/account',  to: 'devise/registrations#create'
+  #   get    '/register', to: 'devise/registrations#new',    as: :new_user_registration
+  #   get    '/account',  to: 'devise/registrations#edit',   as: :edit_user_registration
+  #   patch  '/account',  to: 'devise/registrations#update', as: :user_registration
+  #   get    '/account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+  #   # passwords
+  #   get   'new-pass',  to: 'devise/passwords#new',    as: :new_user_password
+  #   get   'edit-pass', to: 'devise/passwords#edit',   as: :edit_user_password
+  #   patch 'edit-pass', to: 'devise/passwords#update', as: :user_password_update
+  #   post  'new-pass',  to: 'devise/passwords#create', as: :user_password
+  # end
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
