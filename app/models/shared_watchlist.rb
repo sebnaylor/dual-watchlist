4cb245e5228c2204f85aa400e0cc9b120b5326b3
Seb Naylor <sebnaylor@gmail.com>
2025-01-29 15:45:55 +0000

    dont destroy personal watchlist when shared watchlist is destroyed

--- app/models/shared_watchlist.rb
+++ app/models/shared_watchlist.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SharedWatchlist < ApplicationRecord
-  has_many :personal_watchlists, dependent: :destroy
+  has_many :personal_watchlists, dependent: nil
   has_many :users, through: :personal_watchlists
   has_many :media_items, class_name: 'WatchlistMediaItem', through: :personal_watchlists, dependent: :destroy
   has_many :media, through: :media_items
